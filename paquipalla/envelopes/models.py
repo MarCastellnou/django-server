76cf63c47afc980e5482da140c3f6db13cb13f94
MarCastellnou <marccastellnou98@gmail.com>
2019-02-26 12:32:57 +0100

    Todo

--- paquipalla/envelopes/models.py
+++ paquipalla/envelopes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+class Envelope(models.Model):
+    amount = models.IntegerField()
+    motivation = models.CharField(max_length=100)
+    company = models.ForeignKey(Company,on_delete= models.CASCADE)
