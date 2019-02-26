a449fffec25b5c9786fa88382210e0957109f9eb
MarCastellnou <marccastellnou98@gmail.com>
2019-02-26 12:38:14 +0100

    Todo

--- paquipalla/envelopes/models.py
+++ paquipalla/envelopes/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Envelope(models.Model):
     amount = models.IntegerField()
     motivation = models.CharField(max_length=100)
     company = models.ForeignKey(Company,on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.company.name,+" "+ self.motivation
